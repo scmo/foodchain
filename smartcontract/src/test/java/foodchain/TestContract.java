24b8d9415773a9176cd928644a3ac7d88a251a40
Thomas Bocek <tom@tomp2p.net>
2017-01-28 14:30:37 +0100

    added rpc client

--- smartcontract/src/test/java/foodchain/TestContract.java
+++ smartcontract/src/test/java/foodchain/TestContract.java
@@ -1,11 +1,20 @@
 
 package foodchain;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.http.HttpService;
+
+import java.util.concurrent.ExecutionException;
 
 public class TestContract {
     @Test
-    public void testMe() {
-
+    public void testVersion() throws ExecutionException, InterruptedException {
+        Web3j web3 = Web3j.build(new HttpService());  // defaults to http://localhost:8545/
+        Web3ClientVersion web3ClientVersion = web3.web3ClientVersion().sendAsync().get();
+        String clientVersion = web3ClientVersion.getWeb3ClientVersion();
+        Assert.assertEquals("Geth/v1.5.7-stable/linux/go1.7.4", clientVersion);
     }
 }
