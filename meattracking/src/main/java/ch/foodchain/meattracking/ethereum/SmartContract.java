d7f9b4e1dd29d89d11f76042b6c73cc300c5b8c3
Sebastian Stephan <sstephan1337@gmail.com>
2017-01-29 10:41:51 +0100

    Change address and location of keystore.

--- meattracking/src/main/java/ch/foodchain/meattracking/ethereum/SmartContract.java
+++ meattracking/src/main/java/ch/foodchain/meattracking/ethereum/SmartContract.java
@@ -24,22 +24,21 @@ import java.util.concurrent.ExecutionException;
 public class SmartContract {
 
     final private static Web3j web3j;
-    final private static String ACCOUNT1 = "UTC--2016-04-28T14-49-53.053152899Z--d2dd8eab1d77732d43c3c079a18e25378b472a0a";
-    final private static String ACCOUNT2 = "UTC--2016-04-28T14-50-13.743620079Z--faec1b67fee0c36ac5570b43ca0716a19f235ce1";
+    final private static String ACCOUNT1 = "UTC--2017-01-29T09-27-15.119552341Z--b0ef2b23dd4a4495c53dbf0c5e481a284564414b";
 
     static {
         web3j = Web3j.build(new HttpService());  // defaults to http://localhost:8545/
     }
 
     public String deploy(String contractName) throws IOException, CipherException, ExecutionException, InterruptedException {
-        Credentials credentials = WalletUtils.loadCredentials("123456", "/home/draft/.ethereum/testnet/keystore/"+ACCOUNT1);
+        Credentials credentials = WalletUtils.loadCredentials("123456", "/home/ubuntu/.ethereum/testnet/keystore/"+ACCOUNT1);
         FoodChain contract = FoodChain.deploy(web3j, credentials, ManagedTransaction.GAS_PRICE, Contract.GAS_LIMIT, BigInteger.ZERO, new Utf8String(contractName)).get();
         return contract.getContractAddress();
     }
 
 
     public Address lookupName(String contractName) throws Exception {
-        Credentials credentials = WalletUtils.loadCredentials("123456", "/home/draft/.ethereum/testnet/keystore/"+ACCOUNT1);
+        Credentials credentials = WalletUtils.loadCredentials("123456", "/home/ubuntu/.ethereum/testnet/keystore/"+ACCOUNT1);
         FIFSRegistrar fifs = FIFSRegistrar.load("0x2bc4c198325853db29293b6a62f36a62f7cbcf45", web3j, credentials, ManagedTransaction.GAS_PRICE, Contract.GAS_LIMIT);
         return fifs.lookup(new Utf8String(contractName)).get();
     }
@@ -50,7 +49,7 @@ public class SmartContract {
     }
 
     public void updateWithAddress(String adr, String cowId,  String farmAddress, int stepCounter, int outsideTemp, int insideTemp) throws Exception {
-        Credentials credentials = WalletUtils.loadCredentials("123456", "/home/draft/.ethereum/testnet/keystore/"+ACCOUNT1);
+        Credentials credentials = WalletUtils.loadCredentials("123456", "/home/ubuntu/.ethereum/testnet/keystore/"+ACCOUNT1);
         FoodChain contract = FoodChain.load(adr, web3j, credentials, ManagedTransaction.GAS_PRICE, Contract.GAS_LIMIT);
         //contract.update()
         byte[] tmp1 = new byte[32];
@@ -68,7 +67,7 @@ public class SmartContract {
     }
 
     public Cow cowWithAddress(String adr, String cowId) throws Exception {
-        Credentials credentials = WalletUtils.loadCredentials("123456", "/home/draft/.ethereum/testnet/keystore/"+ACCOUNT1);
+        Credentials credentials = WalletUtils.loadCredentials("123456", "/home/ubuntu/.ethereum/testnet/keystore/"+ACCOUNT1);
         FoodChain contract = FoodChain.load(adr, web3j, credentials, ManagedTransaction.GAS_PRICE, Contract.GAS_LIMIT);
         byte[] tmp = new byte[32];
         System.arraycopy(cowId.getBytes(), 0, tmp, 0, cowId.getBytes().length);
